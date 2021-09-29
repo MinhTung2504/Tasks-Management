a485dae6e7c35881ce3216e61e13ea5f9f58d60e
MinhTung2504 <minhtungg2504@gmail.com>
2021-09-29 14:26:54 +0700

    Hashing Password with Salt. Feature Sign-In

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,15 +1,24 @@
 import {
   ConflictException,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<string> {
-    const user = this.create(authCredentialsDto);
+  async registerUser(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    // Hash Password with Salt
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
     try {
       await this.save(user);
     } catch (error) {
@@ -21,5 +30,17 @@ export class UsersRepository extends Repository<User> {
       }
     }
     return 'Successfully!';
+  }
+
+  async loginUser(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'Login Successful!';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
